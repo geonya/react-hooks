bda311c6295c7606bec0491440c8a88b326f24a4
geonya <geony8410@gmail.com>
2022-02-20 22:26:48 +0900

    useConfirm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { useEffect, useRef } from "react";
+const useConfirm = (message = "", callback, rejection) => {
+	if (typeof callback !== "function") return;
 
-const useClick = (onClick) => {
-	const element = useRef();
-	useEffect(() => {
-		if (element.current) {
-			element.current.addEventListener("click", onClick);
+	const confirmAction = () => {
+		if (window.confirm(message)) {
+			callback();
+		} else {
+			rejection();
 		}
-		return () => {
-			if (element.current) {
-				element.current.removeEventListener("click", onClick);
-			}
-		};
-	});
-	return element;
+	};
+	return confirmAction;
 };
-
 const App = () => {
-	const sayHello = () => console.log("hello");
-	const title = useClick(sayHello);
+	const deleteWorld = () => console.log("Deleting the world");
+	const abort = () => console.log("Aborted");
+	const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
 	return (
 		<div className="App">
-			<h1 ref={title}>Hello</h1>
+			<button onClick={confirmDelete}>Delete the world</button>
 		</div>
 	);
 };
