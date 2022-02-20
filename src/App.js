84f202c50b59785c88c8ddc365f12f200de46717
geonya <geony8410@gmail.com>
2022-02-20 11:51:51 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
+export const useInput = (initialValue, validator) => {
+	const [value, setValue] = useState(initialValue);
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+
+		let willUpdate = true;
+		if (typeof validator === "function") {
+			willUpdate = validator(value);
+		}
+		if (willUpdate) {
+			setValue(value);
+		}
+	};
+	return { value, onChange };
+};
+
 function App() {
-	return <div className="App"></div>;
+	const maxLen = (value) => !value.includes("@");
+	const name = useInput("Mr.", maxLen);
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<input placeholder="Name" {...name} />
+		</div>
+	);
 }
 
 export default App;
