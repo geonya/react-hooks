77a0ccfca39afc8b8a03f1063715eb035024139a
geonya <geony8410@gmail.com>
2022-02-20 12:10:13 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import { useState } from "react";
 
-export const useInput = (initialValue, validator) => {
-	const [value, setValue] = useState(initialValue);
-	const onChange = (event) => {
-		const {
-			target: { value },
-		} = event;
+const content = [
+	{
+		tab: "section 1",
+		content: "I'm the content of the section1 ",
+	},
+	{
+		tab: "section 2",
+		content: "I'm the content of the section2 ",
+	},
+];
 
-		let willUpdate = true;
-		if (typeof validator === "function") {
-			willUpdate = validator(value);
-		}
-		if (willUpdate) {
-			setValue(value);
-		}
-	};
-	return { value, onChange };
+const useTabs = (initialTab, allTabs) => {
+	if (!allTabs || !Array.isArray(allTabs)) {
+		return;
+	}
+	const [currentIndex, setCurrentIndex] = useState(initialTab);
+	return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
 };
 
 function App() {
-	const maxLen = (value) => !value.includes("@");
-	const name = useInput("Mr.", maxLen);
+	const { currentItem, changeItem } = useTabs(1, content);
 	return (
 		<div className="App">
-			<h1>Hello</h1>
-			<input placeholder="Name" {...name} />
+			{content.map((section, index) => (
+				<button onClick={() => changeItem(index)} key={index}>
+					{section.tab}
+				</button>
+			))}
+			<div>{currentItem.content}</div>
 		</div>
 	);
 }
